f67a89f12906ee7cc218cf88d515115a3ba1e30e
Benoit Eudier <benoit.eudier@gmail.com>
2018-10-23 07:39:37 +0900

    Define snoozraft msg and add unit test

--- tests/protocol_test.cc
+++ tests/protocol_test.cc
@@ -1,4 +1,21 @@
 //
 // Created by benoit on 18/10/23.
 //
+#include <gtest/gtest.h>
+#include "protocol/message.h"
+#include "protocol/messages/PeerListMessage.h"
+
+using namespace snooz;
+
+TEST(protocol_pack, peer_list) {
+    std::vector<std::string> peers = {"peer1", "peer2", "peer3"};
+    Message msg{std::make_unique<PeerListMessage>(peers)};
+
+    auto zmq_msg = msg.pack("address");
+    auto frames = zmq_msg.frames();
+    ASSERT_EQ(3, frames.size());
+    // Peer list is position 0 in the enum
+    ASSERT_STREQ("address", frames[0].c_str());
+    ASSERT_STREQ("0", frames[1].c_str());
+}
 
